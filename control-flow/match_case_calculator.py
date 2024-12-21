99017e3724f8af0ce0fd673177c79825f86041f4
Wave08 <mararakudakwashe@gmail.com>
2024-12-21 22:16:30 +0200

    Made several Improvements to code including: Error handling, and intergration of a while loop, also asking if the user wants to perform further calculation!

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,22 +1,42 @@
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-
-operator = (input("Choose the operation (+, -, *, /): "))
-
-
-if operator == "+":
-    result = num1 + num2
-    print("The result is",result)
-elif operator == "-":
-    result = num1 - num2
-    print("The result is",result)
-elif operator == "*":
-    result = num1 * num2
-    print("The result is",result)
-elif operator == "/":
+while True:
     try:
-        result = num1 / num2
-        print("The result is ", result)
-    except ZeroDivisionError:
-        print("Cannot divide by zero")
+        num1 = int(input("\nEnter the first number: "))
+        num2 = int(input("Enter the second number: "))
+    except ValueError as ve:
+        print("Error: ",ve)
+        continue
+    
+
+    
+    operator = (input("\nChoose the operation (+, -, *, /): "))
+
+
+    if operator == "+":
+        result = num1 + num2
+        print("The result is",result)
+    elif operator == "-":
+        result = num1 - num2
+        print("The result is",result)
+    elif operator == "*":
+        result = num1 * num2
+        print("The result is",result)
+    elif operator == "/":
+        try:
+            result = num1 / num2
+            print("The result is ", result)
+        except ZeroDivisionError as zd:
+            print("Error:", zd)
+            continue
+    else:
+        print("That is an invalid operator")
+        continue
+    
+    # Prompt if the user still wants to perform another calculation
+    another = input("Do you want to perform another calculation: ").strip().lower()
+    
+    if another != "yes":
+        print("Goodbye")
+        break
+
+
     
