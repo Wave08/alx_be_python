45782e85ff99351d9fcff7967d525e8ac49e9c87
Wave08 <mararakudakwashe@gmail.com>
2024-12-22 13:24:20 +0200

    Added error handling and a while loop which breaks when the user doesn't want any more tables

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,19 @@
-number = int(input("Enter a number to see it's multiplication table: "))
+while True:
+    try:
+        number = int(input("Enter a number to see it's multiplication table: "))
+    
+        for num in range(1, 11):
+            product = number * num
+            print(f"{number} * {num} = ",product)
+            
+        another = input("Do you want to see another multiplication table: ").strip().lower()
+        if another != "yes":
+            print("Goodbye")
+            break
+        
+    except ValueError as ve:
+        print(ve)
+        continue
+    
 
-for num in range(1, 11):
-    product = number * num
-    print(f"{number} * {num} = ",product)
+
